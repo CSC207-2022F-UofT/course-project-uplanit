5ed2a6b1789a067a6cddfbade50283a306dee010
Riley McCarron <riley.mccarron@live.ca>
2022-11-14 13:18:07 -0500

    started the event entity - riley

---
+++ src/main/java/Event.java
@@ -0,0 +1,14 @@
+import java.sql.Time;
+import java.util.Date;
+
+public class Event {
+    public String name;
+    public Time commuteTime = new Time(0);
+    public Date eventStart;
+    public Date eventEnd;
+    // default for location is null
+    public String location;
+
+
+
+}
