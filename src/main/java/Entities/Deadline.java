1cd069a7f744d050e9a1b94a1b723578d954785f
Rodrigo Reyes <rodrigo.reyesferegrino@mail.utoronto.ca>
2022-11-18 19:47:50 -0500

    created constructor and instance attributes, but this time did commit

--- src/main/java/Entities/Deadline.java
+++ src/main/java/Entities/Deadline.java
@@ -1,2 +1,17 @@
-package Entities;public class Deadline {
+/*
+This is the Deadline Entity; It stores information for all the deadlines
+ */
+
+package java.Entities;
+import java.sql.Time;
+import java.util.Date;
+public class Deadline {
+    public String name;
+    public Date date;
+    public Deadline(String name, Date date){
+        this.name = name;
+        this.date = date;
+
+
+    }
 }
