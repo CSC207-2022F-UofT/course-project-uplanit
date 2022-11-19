8c795786405abb5e31f279ed9c8696466a40e26f
Yuki-Or <kkrr135792468@gmail.com>
2022-11-18 20:25:44 -0500

    [Feature 4] Changed methods based on updated Event Class

--- src/main/java/ModifyEvent.java
+++ src/main/java/ModifyEvent.java
@@ -1,25 +1,41 @@
+import java.sql.Time;
 import java.util.Date;
 public class ModifyEvent {
     // this part changes a lot depending on how the Event Class is implemented
-    Event event = new Event();
+    Event event;
 
     public ModifyEvent(Event e){
         event = e;
     }
 
     public void ChangeEventStart(Date new_start){
-        event.eventStart = new_start;
+        event.startTime = new_start;
     }
 
     public void ChangeEventEnd(Date new_end){
-        event.eventEnd = new_end;
+        event.endTime = new_end;
     }
 
     public void ChangeLocation(String new_location){
-        event.location = new_location;
+
+        // is location default null or empty string?
+
+        // if this event object contains location:
+        if(event.location == null) {
+            event.location = new_location;
+        }
     }
 
     public void ChangeName(String new_name){
-        event.name = new_name;
+            event.name = new_name;
+    }
+
+    public void ChangeCommuteTime(Time new_commuteTime){
+        Time default_time = new Time(0);
+
+        // if this event object contains commute time:
+        if(new_commuteTime != default_time) {
+            event.commuteTime = new_commuteTime;
+        }
     }
 }
