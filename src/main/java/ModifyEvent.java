e562f014d6b32e5a3a58e2d3f3d077720c8739d1
Yuki-Or <kkrr135792468@gmail.com>
2022-11-18 20:06:08 -0500

    [Feature 4] Added methods for modifying event info

--- src/main/java/ModifyEvent.java
+++ src/main/java/ModifyEvent.java
@@ -1,3 +1,25 @@
+import java.util.Date;
 public class ModifyEvent {
-    // test test:0
+    // this part changes a lot depending on how the Event Class is implemented
+    Event event = new Event();
+
+    public ModifyEvent(Event e){
+        event = e;
+    }
+
+    public void ChangeEventStart(Date new_start){
+        event.eventStart = new_start;
+    }
+
+    public void ChangeEventEnd(Date new_end){
+        event.eventEnd = new_end;
+    }
+
+    public void ChangeLocation(String new_location){
+        event.location = new_location;
+    }
+
+    public void ChangeName(String new_name){
+        event.name = new_name;
+    }
 }
