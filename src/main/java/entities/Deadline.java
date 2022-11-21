ffc8ec1ceb3c79d54ff0ee4ec54377a602d198cf
Rodrigo Reyes <rodrigo.reyesferegrino@mail.utoronto.ca>
2022-11-21 13:23:21 -0500

    corrected a syntax error

--- src/main/java/entities/Deadline.java
+++ src/main/java/entities/Deadline.java
@@ -9,7 +9,9 @@ import java.util.Date;
 public class Deadline {
     public String name;
     public Date date;
-    public Deadline(String name, Date date){
+
+    public Deadline(String name, Date date) {
         this.name = name;
         this.date = date;
+    }
 }
