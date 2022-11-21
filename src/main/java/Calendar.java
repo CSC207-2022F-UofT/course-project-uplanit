70ed393a34b28d2a29801d392568b291e762a1ef
Yuki-Or <kkrr135792468@gmail.com>
2022-11-21 12:29:31 -0500

    Fixed them to CamelCase

--- src/main/java/Calendar.java
+++ src/main/java/Calendar.java
@@ -6,22 +6,22 @@ import java.util.List;
 
 public class Calendar {
     // This depends on how the Week Class is implemented. Change the ArrayList's content Type if necessary.
-    public ArrayList<Map<Event, Date>> week_maps; // a list of week-map
+    public ArrayList<Map<Event, Date>> weekMaps; // a list of week-map
 
 
     public Calendar() {
-         week_maps= new ArrayList<Map<Event,Date>>();
+         weekMaps= new ArrayList<Map<Event,Date>>();
     }
 
     public void StoreWeekMaps(Map<Event, Date> week) {
-        week_maps.add(week);
+        weekMaps.add(week);
     }
 
     public String AlertConflict() {
-        for(int i = 0; i < week_maps.size(); i++){
+        for(int i = 0; i < weekMaps.size(); i++){
             // again, this part depends on how the Week Class is implemented
             // once you have the Week Class, replace if condition with relevant code
-            if(i == week_maps.size() - 1) {
+            if(i == weekMaps.size() - 1) {
                 return "bruh there's conflict fix it";
             }
         }
