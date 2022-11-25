a2a420634bc44377a06c09e946c93162d806a744
malu-brisi <malu.brisighello@mail.utoronto.ca>
2022-11-24 22:45:52 -0500

    Made Deadline attributes private and added their respective getters and setters.

--- src/main/java/entities/Deadline.java
+++ src/main/java/entities/Deadline.java
@@ -8,11 +8,27 @@ package entities;
 import java.util.Date;
 
 public class Deadline {
-    public String name;
-    public Date date;
+    private String name;
+    private Date date;
 
     public Deadline(String name, Date date) {
         this.name = name;
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
         this.date = date;
     }
 }
