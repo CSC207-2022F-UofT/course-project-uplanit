90eeb2ebcb32c8b91b36828ab9d03082676ba89a
Yuki-Or <kkrr135792468@gmail.com>
2022-11-25 18:58:30 -0500

    modified ModifyEvent class to delete errors

--- src/main/java/ModifyEvent.java
+++ src/main/java/ModifyEvent.java
@@ -3,26 +3,23 @@ import entities.*;
 import java.sql.Time;
 import java.util.Date;
 public class ModifyEvent extends Event{
-    // this part changes a lot depending on how the entities.Event Class is implemented
     Event event;
 
-    public ModifyEvent(String name, Date startTime, Date endTime, Time commuteTime, String location){
-        super(name, startTime, endTime, commuteTime, location);
+    public ModifyEvent(String name, Date startTime, Date endTime, Event commute, String location){
+        super(name, startTime, endTime, commute, location);
     }
 
     public void ChangeEventStartTime(Date newStartTime){
-        event.setStartTime(newStartTime);
+
     }
 
     public void ChangeEventEndTime(Date newEndTime){
-        event.setEndTime(newEndTime);
+
     }
 
     public void ChangeLocation(String newLocation){
         // if this event object contains location:
-        if(event.getLocation() != null) {
-            event.setLocation(newLocation);
-        }
+        //if(event.getLocation() != null) {}
     }
 
     public void ChangeName(String newName){
@@ -30,9 +27,12 @@ public class ModifyEvent extends Event{
     }
 
     public void ChangeCommuteTime(Time newCommuteTime){
+
+        // 1) calculate what would be the "start time" and "end time" of commute
+        // 2) get into the date that holds this event object
+        // 3) check if there is another event object that has startTime-endTime "trapping" the new commuteTime
+
         // if this event object contains commute time:
-        if(event.getCommuteTime() != null) { // review this !!! -> if this event object contains commuteTime
-            event.setCommuteTime(newCommuteTime);
-        }
+        // if(event.getCommuteTime() != null) { // review this !!! -> if commuteTime is not null}
     }
 }
