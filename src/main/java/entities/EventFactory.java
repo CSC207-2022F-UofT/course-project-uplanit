d798572dc6a4ae7be9ae393422710d883ce60792
Riley McCarron <riley.mccarron@live.ca>
2022-11-26 22:14:15 -0500

    added a docstring because they make comfy.

--- src/main/java/entities/EventFactory.java
+++ src/main/java/entities/EventFactory.java
@@ -5,6 +5,19 @@ import java.util.Date;
 
 class EventFactory {
 
+
+    /**
+     * Returns a newly constructed Event object based on the given input. If eventType is null then it returns null.
+     * If an invalid eventType input is given then an exception is thrown.
+     *
+     * @param eventType The type of the event to construct (single, recurrent, or dynamic)
+     * @param name the name of the event to construct
+     * @param startTime the time that the desired event starts at
+     * @param endTime the time that the desired event ends at
+     * @param commute the commute time that corresponds to this event
+     * @param location the location that the constructed event takes place at
+     * @return the desired event, if valid. null if eventType is null.
+     */
     public static Event getEvent(String eventType,
                           String name, Date startTime,
                           Date endTime, Event commute,
