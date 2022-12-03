e5a55230bd89dfab4a81667a5177905765d278ed
Selen Bayram <selen.bayram@mail.utoronto.ca>
2022-12-02 20:56:33 -0500

    Added two of my use case files

---
+++ src/main/java/use_cases/add_dynamic_event_use_case/AddDynamicEventInputBoundary.java
@@ -0,0 +1,6 @@
+package use_cases.add_dynamic_event_use_case;
+
+// Use case layer
+
+public interface AddDynamicEventInputBoundary {
+}
---
+++ src/main/java/use_cases/add_dynamic_event_use_case/AddDynamicEventInteractor.java
@@ -0,0 +1,6 @@
+package use_cases.add_dynamic_event_use_case;
+
+// Use case layer
+
+public class AddDynamicEventInteractor implements AddDynamicEventInputBoundary {
+}
