9ce400373dd0b57fa1ba6653316fc53402a26272
Riley McCarron <riley.mccarron@live.ca>
2022-12-03 03:44:42 -0500

    Turned EventFactory into an interface instead of a class. Different class implementation for each type of event.
    Replaced any use of Boolean object with boolean primitive.
    
    Got rid of some unused imports in the entities.
    
    Created new use case files without implementation.
    
    Created The RecurrentEventDsGateway interface without implementation.

--- src/main/java/use_cases/recurrent_event_use_case/RecurrentEventDsGateway.java
+++ src/main/java/use_cases/recurrent_event_use_case/RecurrentEventDsGateway.java
@@ -2,5 +2,11 @@ package use_cases.recurrent_event_use_case;
 
 // Use case layer
 
+import java.time.LocalDateTime;
+
 public interface RecurrentEventDsGateway {
+
+    boolean hasConflict(LocalDateTime startTime, LocalDateTime endTime);
+
+
 }
---
+++ src/main/java/use_cases/recurrent_event_use_case/RecurrentEventDsRequestModel.java
@@ -0,0 +1,20 @@
+package use_cases.recurrent_event_use_case;
+import java.time.LocalDateTime;
+
+// Use class layer
+
+public class RecurrentEventDsRequestModel {
+
+    private final String name;
+    private final LocalDateTime startTime;
+    private final LocalDateTime endTIme;
+    private final int commuteTime;
+    private final boolean isCommute = false;
+    private final location;
+
+    public
+
+
+
+
+}
