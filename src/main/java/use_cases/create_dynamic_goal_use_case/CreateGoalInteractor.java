53bfa2bc964524e3cbe6e8b3fd1fa783fb25445b
Rodri-rf <rodrigo.reyesferegrino@mail.utoronto.ca>
2022-12-06 11:30:02 -0500

    Created package for creating a dynamic goal, as per the packaging structure discussed in our last meeting. Within it are the classes for the Use Case Interactor, the Data structures, and the input boundary interface (which is not strictly necessary in terms of dependency inversion prescribed by SOLID, but will help make things clearer)

--- src/main/java/use_cases/create_dynamic_goal_use_case/CreateGoalInteractor.java
+++ src/main/java/use_cases/create_dynamic_goal_use_case/CreateGoalInteractor.java
@@ -1,6 +1,7 @@
 package use_cases.create_dynamic_goal_use_case;
 
 public class CreateGoalInteractor implements CreateGoalInputBoundary {
+
     public CreateGoalInteractor(InputDataStructure inputData){
     }
 }
