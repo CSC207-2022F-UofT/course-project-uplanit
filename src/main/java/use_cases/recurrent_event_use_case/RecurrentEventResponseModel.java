e5cefc4262420bbedb8cfb0f8408bf132dc159d7
Riley McCarron <riley.mccarron@live.ca>
2022-12-06 23:44:40 -0500

    Got rid of some entities but that is not the main point of this branch that will be decided. Began implementation of recurrent event use case files.
    Particularly the RequestModel in this commit.

--- src/main/java/use_cases/recurrent_event_use_case/RecurrentEventResponseModel.java
+++ src/main/java/use_cases/recurrent_event_use_case/RecurrentEventResponseModel.java
@@ -4,5 +4,24 @@ package use_cases.recurrent_event_use_case;
 
 public class RecurrentEventResponseModel {
 
-    String
+    String name;
+    String message;
+
+    // Construct the response we would like to give to the presenter
+    public RecurrentEventResponseModel(String name, String message) {
+
+        this.name = name;
+        this.message = message;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    // not sure if this needs setters
+
 }
