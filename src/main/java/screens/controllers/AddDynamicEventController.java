7bdb3b1f82d9e65b954ed6f194f725f68806ee0e
Selen Bayram <selen.bayram@mail.utoronto.ca>
2022-12-08 02:24:59 -0500

    worked on add dynamic event

--- src/main/java/screens/controllers/AddDynamicEventController.java
+++ src/main/java/screens/controllers/AddDynamicEventController.java
@@ -1,11 +1,11 @@
 package screens.controllers;
 
-import entities.Event;
 import use_cases.add_dynamic_event_use_case.AddDynamicEventInputBoundary;
 import use_cases.add_dynamic_event_use_case.AddDynamicEventRequestModel;
 import use_cases.add_dynamic_event_use_case.AddDynamicEventResponseModel;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 // Interface Adapters layer; Controller for AddDynamicEvent use case
 public class AddDynamicEventController {
@@ -22,21 +22,25 @@ public class AddDynamicEventController {
     }
 
     /**
-     * Return AddDynamicEventResponseModel object with given event_name, event start_time, event end_time, is_commute,
-     * commute, and location.
+     * Return AddDynamicEventResponseModel object with given event_name, event start_time_str, event end_time_str,
+     * is_commute_str, commute_str, and location.
      *
      * @param event_name the name of this event
-     * @param start_time the start time for this event
-     * @param end_time the end time for this event
-     * @param is_commute whether this event requires commute
-     * @param commute the event representing the commute
+     * @param start_time_str the start time for this event
+     * @param end_time_str the end time for this event
      * @param location the location of this event
      * @return the response model for the event
      */
-    AddDynamicEventResponseModel create(String event_name, LocalDateTime start_time, LocalDateTime end_time,
-                                        boolean is_commute, Event commute, String location) {
-        AddDynamicEventRequestModel requestModel = new AddDynamicEventRequestModel(
-                event_name, start_time, end_time, is_commute, commute, location);
+    AddDynamicEventResponseModel create(String event_name, String start_time_str, String end_time_str,
+                                        String location) {
+
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yy HH:mm");
+        LocalDateTime start_time = LocalDateTime.parse(start_time_str, formatter);
+        LocalDateTime end_time = LocalDateTime.parse(end_time_str, formatter);
+
+        AddDynamicEventRequestModel requestModel = new AddDynamicEventRequestModel(event_name, start_time,
+                end_time, false, null, location);
 
         // Calls the create method that is defined in the AddDynamicEventInputBoundary interface and implemented
         // in the AddDynamicEventInteractor to return AddDynamicEventResponseModel object.
